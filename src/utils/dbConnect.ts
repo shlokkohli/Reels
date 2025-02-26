0327b002da38ed3a2c5ad2b21eb84b30cbb5af2e
Shlok Kohli <shlokkohli11@gmail.com>
2025-02-26 23:24:11 +0530

    Database connection made

---
+++ src/utils/dbConnect.ts
@@ -0,0 +1,33 @@
+import mongoose, { Connection } from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+
+    // check if there is already a database connection
+    if(connection.isConnected){
+        console.log("Already connected to database");
+        return;
+    }
+
+    // if there is not already a database connection, we need to create one
+    try {
+
+        const db = await mongoose.connect(process.env.MONGODB_URI || '');
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB Connected Successfully")
+        
+    } catch (error) {
+
+        console.log("Database conenction failed", error);
+        process.exit(1)
+        
+    }
+}
+
+export default dbConnect;
