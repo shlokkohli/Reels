7ce76d69d0fe0f38ea779fb2836eb7270ae1cf65
Shlok Kohli <shlokkohli11@gmail.com>
2025-03-03 02:32:11 +0530

    Completed user registration (Error in successful signup toast)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,102 +1,137 @@
 'use client'
 import { useRouter } from "next/navigation";
 import { useState } from "react"
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import Link from "next/link";
+import { signUpSchema } from "@/schema/signUpSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from 'react-hook-form'
+import { z } from "zod";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner"
 
 function Register() {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
-    const [error, setError] = useState<string | null>("")
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  
+  const router = useRouter()
 
-    const router = useRouter()
-
-    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
-
-      e.preventDefault();
-      if(password !== confirmPassword){
-        setError("Your password does not match")
-      }
-
-      try {
-
-        const response = await axios.post('/api/auth/register', {
-          email, password
-        })
-
-        console.log(response)
-        
-      } catch (error) {
-        console.log(error)
-      }
-
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
     }
+  });
+
+  interface ErrorResponse {
+    error : string
+  }
+  
+  const handleUserRegistration = async (data: z.infer<typeof signUpSchema>) => {
+
+    setIsSubmitting(true)
+
+    try {
+
+      const response = await axios.post('/api/auth/register', data)
+      
+      router.push('/login')
+      
+    } catch (error) {
+
+      const axiosError = error as AxiosError<ErrorResponse>
+      const errorMessage = axiosError.response?.data?.error
+      toast("Registration Failed", {
+        description: errorMessage
+      })
+
+    } finally {
+      setIsSubmitting(false)
+    }
+
+  }
 
   return (
     <div className="max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-4">Register</h1>
 
-      <form onSubmit={handleSubmit} className="space-y-4">
+      <form onSubmit={form.handleSubmit(handleUserRegistration)} className="space-y-4">
 
+        {/* email input */}
         <div>
-          <label htmlFor="email">
+          <label htmlFor="email" className="block mb-1">
             Email
           </label>
           <input
-            type="text"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+            type="email"
             className="w-full px-3 py-2 border rounded"
+            {...form.register('email')}
           />
+          {form.formState.errors.email && (
+            <p className="text-red-500 text-sm">{form.formState.errors.email.message}</p>
+          )}
         </div>
-        
+
+        {/* password input */}
         <div>
-          <label htmlFor="password" className="block mb-1">
+          <label htmlFor="password">
             Password
           </label>
           <input
-            type="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
+            type="password"
             className="w-full px-3 py-2 border rounded"
+            {...form.register('password')}
           />
+          {form.formState.errors.password && (
+            <p className="text-red-500 text-sm">{form.formState.errors.password.message}</p>
+          )}
         </div>
 
+        {/* confirm password */}
         <div>
-          <label htmlFor="confirmPassword" className="block mb-1">
+          <label htmlFor="confirmPassword">
             Confirm Password
           </label>
           <input
-            type="password"
             id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
+            type="password"
             className="w-full px-3 py-2 border rounded"
+            {...form.register('confirmPassword')}
           />
+          {form.formState.errors.confirmPassword && (
+            <p className="text-red-500 text-sm">{form.formState.errors.confirmPassword.message}</p>
+          )}
         </div>
 
         <button
           type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+          className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 disabled:bg-blue-300 disabled:cursor-not-allowed flex items-center justify-center gap-2"
+          disabled={isSubmitting}
         >
-          Register
-        </button>
+          {isSubmitting ? (
+          <>
+            <Loader2 className="h-4 w-4 animate-spin" />
+            <span>Please wait</span>
+          </>
+          ) : (
+            'Register'
+          )}
+          </button>
 
-        <p>
+
+        <p className="text-center mt-4">
           Already have an account?{" "}
-          <Link href='/login' className='text-blue-500 hover:text-blue-600'>
+          <Link href='/login' className="text-blue-500 hover:text-blue-600">
             Login
           </Link>
         </p>
 
       </form>
+
     </div>
   )
 }
--- src/schema/signUpSchema.ts
+++ src/schema/signUpSchema.ts
@@ -2,5 +2,9 @@ import { z } from 'zod'
 
 export const signUpSchema = z.object({
     email: z.string().email({ message : "Invalid email address" }),
-    password: z.string().min(6, { message: "Password must be atleast 6 characters" })
+    password: z.string().min(6, { message: "Password must be atleast 6 characters" }),
+    confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ['confirmPassword']
 })
