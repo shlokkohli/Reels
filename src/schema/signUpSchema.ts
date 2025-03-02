823575c0ec6dfa4e315e5298fbc8226b05668a9f
Shlok Kohli <shlokkohli11@gmail.com>
2025-03-03 00:10:00 +0530

    created signUpSchema for user registration

---
+++ src/schema/signUpSchema.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod'
+
+export const signUpSchema = z.object({
+    email: z.string().email({ message : "Invalid email address" }),
+    password: z.string().min(6, { message: "Password must be atleast 6 characters" })
+})
