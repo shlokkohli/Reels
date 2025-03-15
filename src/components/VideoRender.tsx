455e9d5f532c8a6d5f80eb8c97151a732b405abe
Shlok Kohli <shlokkohli11@gmail.com>
2025-03-16 04:04:29 +0530

    Changes made in the VideoRender page

--- src/components/VideoRender.tsx
+++ src/components/VideoRender.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Video } from '@/models/Video'
 import { IKVideo } from 'imagekitio-next'
+import Link from 'next/link'
 import React from 'react'
 
 interface videoDataType {
@@ -9,11 +10,27 @@ interface videoDataType {
 
 const VideoRender = ({video} : videoDataType) => {
 
-  console.log(video.videoURL)
+  console.log("this is the id: ",video.videoURL)
 
   return (
-    <div>
-      
+    <div className='card bg-base-100 shadow hover:shadow-lg transition-all duration-300'>
+      <figure className='relative px-4 pt-4'>
+        <Link href={`/videos/${video._id}`} className='relative group w-full'>
+          <div className='rounded-xl overflow-hidden relative w-full' style={{ aspectRatio: "9/16" }}>
+            <IKVideo
+              path={video.videoURL}
+              transformation={[
+                {
+                  height: '1920',
+                  width: '1080',
+                },
+              ]}
+              controls={video.controls}
+              className='w-full h-full object-cover'
+            />
+          </div>
+        </Link>
+      </figure>
     </div>
   )
 }
